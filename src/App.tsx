d3d91cce3a0093d2ad7900ac99c7145f9a474554
CottonLeSergal <temperdox@gmail.com>
2021-03-13 07:31:58 -0800

    Added Tab component for dynamic creation of tabs.

--- src/App.tsx
+++ src/App.tsx
@@ -25,16 +25,6 @@ function App() {
       ]
     }
   ]
-
-  /*let j = [
-    {
-      src: logo,
-      alt: "J",
-      title: "J",
-      content: ["search","vaadin","garbage collection"],
-      buttons: ["Github","Github","Github"]
-    }
-  ]*/
   let j = [
     {
       src: logo,
@@ -56,33 +46,40 @@ function App() {
       ]
     }
   ]
-  let tabContent  = [c,j];
+  let r = [
+    {
+      src: logo,
+      alt: "R",
+      title: "R",
+      content: [
+        {
+          title: "Search",
+          button: {title:"Github",href:"www.github.com"}
+        },
+        {
+          title: "Vaadin",
+          button: {title:"Github",href:"www.github.com"}
+        },
+        {
+          title: "Garbage Collection",
+          button: {title:"Github",href:"www.github.com"}
+        }
+      ]
+    }
+  ]
+  let tabContent  = [c,j,r];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <>
         {
           tabContent.map(tab =>(
-            tab.map(content =>(
-                <>
-                  <Tab src={content.src} alt={content.alt} title={content.title} content={content.content}/>
-                </>
-            ))
+              tab.map(content =>(
+                  <>
+                    <Tab src={content.src} alt={content.alt} title={content.title} content={content.content}/>
+                  </>
+              ))
           ))
         }
-      </header>
-    </div>
+      </>
   );
 }
 
--- src/Tab.js
+++ src/Tab.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Tab.css';
 import './App.css';
 
-function Tabs(props){
-    console.log("Alt = " + props.isExtended)
-    /*console.log("Height: " + props.sty['height'])*/
+function Tab(props){
+    const [showMessage, setShowMessage] = useState(false);
+    let name = ''
+    if(showMessage){
+        name = '.extended'
+    }else{name=''}
     return(
         <>
-            <p>Height:{props.height['height']}</p>
-            <div className={"tabContent"} style={props.height}>
+            <div className="tab">
+                <div onClick={() => setShowMessage(!showMessage)} className={"tabHeader" + name}>
+                    <img src={props.src} alt={props.alt} style={{width: 50, marginLeft: 20}}/>
+                    <h3 className="tabHeaderTitle">{props.title}</h3>
+                </div>
+            </div>
+            <div className={'tabContent' + name}>
                 {
                     props.content.map(item => (
                         <li className="tabListItem">{item.title} <a href={item.button.href}
@@ -19,41 +27,7 @@ function Tabs(props){
                 }
             </div>
         </>
-
     )
-}
-
-class Tab extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {isExtended: false, sty:{height: 0 + 'px'}}
-        console.log("Console Test: " + this.state.sty['height'])
-        this.handleToggleClick = this.handleToggleClick.bind(this);
-    }
-
-    handleToggleClick(){
-        this.setState(state =>({
-            isExtended: !state.isExtended
-        }));
-        this.setState(state =>({
-            /*height: (state.height === 'auto !important') ? '0px' : 'auto !important'*/
-            sty : (state.sty['height'] === 0 + 'px') ? {height: 'auto !important'} : {height: '0px'}
-        }))
-        console.log(this.state.isExtended)
-        console.log("height: " + this.state.sty['height'])
-    }
-
-    render(){
-        return(
-            <div className="tab">
-                <div onClick={this.handleToggleClick} className="tabHeader">
-                    <img src={this.props.src} alt={this.props.alt} style={{width: 50, marginLeft: 20}}/>
-                    <h3 className="tabHeaderTitle">{this.props.title}</h3>
-                </div>
-                <Tabs content={this.props.content} isExtended={this.state.isExtended} height={this.state.sty}/>
-            </div>
-        )
-    }
 }
 
 export default Tab
